f7fd95b25be20f7530009cf366131074846a8af0
notuya <notuya0@gmail.com>
2026-05-11 17:00:38 -0400

    Update PatientPriorityQueue.java

--- src/PatientPriorityQueue.java
+++ src/PatientPriorityQueue.java
@@ -1,1 +1,29 @@
+import java.util.PriorityQueue;
+public class PatientPriorityQueue {
 
+    private PriorityQueue<String> queue;
+    public PatientPriorityQueue() {
+
+        queue = new PriorityQueue<>();
+    }
+
+    // INSERT PATIENT
+    public void insertPatient(String patientType, String patientName) {
+
+        // Emergency patients get E
+        // Normal patients get N
+        if (patientType.equals("Emergency")) {
+            queue.add("E -> " + patientName);
+        }
+        else {
+            queue.add("N -> " + patientName);
+        }
+    }
+    // PROCESS PATIENTS
+    public void processPatients() {
+        while (!queue.isEmpty()) {
+            String patient = queue.poll();
+            System.out.println(patient);
+        }
+    }
+}
