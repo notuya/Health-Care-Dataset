ee1bc60eb710ad79bd994d36369a875462c58581
nangyalaiS <sultanin0827@students.bowiestate.edu>
2026-05-11 17:14:47 -0400

    Update AdmissionSystem.java

--- src/AdmissionSystem.java
+++ src/AdmissionSystem.java
@@ -1,1 +1,20 @@
-
+import java.util.PriorityQueue;
+public class AdmissionSystem {
+// The queue will now automatically use the compareTo method inside
+PatientRecord
+private PriorityQueue<PatientRecord> queue = new PriorityQueue<>();
+public void admitPatient(PatientRecord r) {
+queue.add(r);
+}
+public PatientRecord processNext() {
+// Returns the patient with the highest priority (Emergency
+first)
+return queue.poll();
+}
+public void displayQueue() {
+for (PatientRecord p : queue) {
+System.out.println(p.getRecordID() + " - " +
+p.getAdmissionType());
+    }
+  }
+}
