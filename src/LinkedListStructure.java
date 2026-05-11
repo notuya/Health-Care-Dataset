41cae18073515b67d2c944b33763d28fc91da581
Joseph Thomas <THOMASJ0306@students.bowiestate.edu>
2026-05-11 14:47:11 -0400

    LinkedListed implementation
    created a LinkedListStructure with CRUD operations to use project.

--- src/LinkedListStructure.java
+++ src/LinkedListStructure.java
@@ -1,1 +1,67 @@
-
+public class LinkedListStructure {
+  private Node head;
+  
+  class Node {
+    PatientRecord data;
+    Node next;
+    public Node(PatientRecord data) {
+      this.data = data;
+      next = null;
+    }
+  }
+  
+  // insert
+  public void insertRecord(PatientRecord record) {
+    Node newNode = new Node(record);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+    
+    Node temp = head;
+      while (temp.next != null) {
+        temp = temp.next;
+      }
+    temp.next = newNode;
+  }
+  
+// search
+  public PatientRecord searchRecord(int id) {
+    Node temp = head;
+    while (temp != null) {
+      if (temp.data.getRecordID() == id) {
+        return temp.data;
+      }
+      temp = temp.next;
+    }
+    return null;
+  }
+  
+// delete
+  public void deleteRecord(int id) {
+    if (head == null) {
+      return;
+    }
+    
+    if (head.data.getRecordID() == id) {
+      head = head.next;
+      return;
+    }
+    Node temp = head;
+    while (temp.next != null) {
+      if (temp.next.data.getRecordID() == id) {
+        temp.next = temp.next.next;
+        return;
+      }
+      temp = temp.next;
+    }
+  }
+// traversal
+  public void traverseRecords() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.println(temp.data);
+      temp = temp.next;
+    }
+  }
+}
