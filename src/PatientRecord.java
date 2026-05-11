c500ad3a9c25337004721012f6597c573c60c904
notuya <notuya0@gmail.com>
2026-05-11 16:58:33 -0400

    Create PatientRecord class with patient details
    Added a PatientRecord class to manage patient data.

--- src/PatientRecord.java
+++ src/PatientRecord.java
@@ -1,1 +1,43 @@
+public class PatientRecord {
 
+    private int recordID;
+    private int age;
+    private String gender;
+    private String medicalCondition;
+    private String hospital;
+    private String admissionType;
+    private double billingAmount;
+
+    public PatientRecord(int recordID, int age, String gender, String medicalCondition, String hospital, String admissionType,
+        double billingAmount) {
+
+        this.recordID = recordID;
+        this.age = age;
+        this.gender = gender;
+        this.medicalCondition = medicalCondition;
+        this.hospital = hospital;
+        this.admissionType = admissionType;
+        this.billingAmount = billingAmount;
+    }
+
+    public int getRecordID() {
+        return recordID;
+    }
+
+    public String getAdmissionType() {
+        return admissionType;
+    }
+
+    public String toString() {
+
+        return "ID: " + recordID +
+                " Age: " + age +
+                " Gender: " + gender +
+                " Condition: " + medicalCondition +
+                " Hospital: " + hospital +
+                " Admission: " + admissionType +
+                " Billing: $" + billingAmount;
+    }
+}
+
+//Can explain
