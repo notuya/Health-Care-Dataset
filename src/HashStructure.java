a82178e01958a6cea113cc569deece02849c4f39
Joseph Thomas <THOMASJ0306@students.bowiestate.edu>
2026-05-11 16:31:50 -0400

    Implement HashStructure for PatientRecord management

--- src/HashStructure.java
+++ src/HashStructure.java
@@ -1,1 +1,30 @@
+import java.util.HashMap;
 
+public class HashStructure {
+  private HashMap<Integer, PatientRecord> map;
+  public HashStructure() {
+    map = new HashMap<>();
+  }
+  
+// insert
+  public void insertRecord(PatientRecord record) {
+    map.put(record.getRecordID(), record);
+  }
+  
+// search
+  public PatientRecord searchRecord(int id) {
+    return map.get(id);
+  }
+  
+// delete
+  public void deleteRecord(int id) {
+    map.remove(id);
+  }
+  
+// traversal
+  public void traverseRecords() {
+    for (PatientRecord record : map.values()) {
+      System.out.println(record);
+    }
+  }
+}
